407626a0f709908583c09b29d536837eec888fcb
Parthiv Naresh <pnaresh.github@gmail.com>
2022-04-13 01:37:29 -0400

    final coverage

--- facilyst/tests/models_tests/test_model_base.py
+++ facilyst/tests/models_tests/test_model_base.py
@@ -1,28 +1,7 @@
-from facilyst.models import ModelBase
+from facilyst.models import RandomForestRegressor
 
 
 def test_equals():
-    class MockPerceptronRegressor(ModelBase):
-        name = "Mock Neural Regressor"
+    rf_regressor = RandomForestRegressor()
 
-        primary_type = "regressor"
-        secondary_type = "neural"
-        tertiary_type = "perceptron"
-
-        hyperparameters = None
-
-        def get_params(self):
-            return {1: "1"}
-
-    class MockTreeRegressor(ModelBase):
-        name = "Mock Tree Regressor"
-        primary_type = "regressor"
-        secondary_type = "ensemble"
-        tertiary_type = "tree"
-
-        hyperparameters = None
-
-        def get_params(self):
-            return {1: "1"}
-
-    assert MockPerceptronRegressor().__eq__(MockTreeRegressor())
+    assert isinstance(rf_regressor.__eq__(4), NotImplemented.__class__)
