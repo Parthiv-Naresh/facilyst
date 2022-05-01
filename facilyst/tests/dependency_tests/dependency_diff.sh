0d6e7fb5b5bf9b77c5661045729eb6e99125ffbc
Parthiv Naresh <pnaresh.github@gmail.com>
2022-05-01 02:42:51 -0400

    update dependency checker to handle extra dependencies

--- facilyst/tests/dependency_tests/dependency_diff.sh
+++ facilyst/tests/dependency_tests/dependency_diff.sh
@@ -1,3 +1,3 @@
-allow_list=$(< requirements.txt grep -oE "^[a-zA-Z0-9]+[a-zA-Z0-9_\-]*" | paste -d "|" -s -)
+allow_list=$(< "${ALLOWED_DEPENDENCY_FILEPATH}" grep -oE "^[a-zA-Z0-9]+[a-zA-Z0-9_\-]*" | paste -d "|" -s -)
 echo "Allow list: ${allow_list}"
 pip freeze | grep -v "facilyst.git" | grep -E "${allow_list}" > "${LATEST_DEPENDENCY_FILEPATH}"
