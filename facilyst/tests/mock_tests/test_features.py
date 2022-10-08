b12430807eebd1132fa262b01ebc162aaf9b71d7
Parthiv Naresh <pnaresh.github@gmail.com>
2022-10-08 13:20:33 -0400

    remove prints

--- facilyst/tests/mock_tests/test_features.py
+++ facilyst/tests/mock_tests/test_features.py
@@ -98,8 +98,6 @@ def test_features_parameters(
         and len(features_included) == 22
         and num_rows != 10
     ):
-        print("CHECKING THE SCHEMA")
-        print(features.ww)
         assert (
             len(
                 features.ww.select(
